27fa5ec5096b0735ee4866d1eff165294d43daed
lucaspomba <141828588+lucaspomba@users.noreply.github.com>
2025-05-05 12:10:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Função para enviar o formulário de opinião
+document.getElementById('opinionForm').addEventListener('submit', function (e) {
+  e.preventDefault();
+  alert('Sua opinião foi enviada!');
+});
